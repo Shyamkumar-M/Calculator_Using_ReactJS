9b916fed4c5d287d128f33c5584087fdfb5ccefe
Shyamkumar-M <139098661+Shyamkumar-M@users.noreply.github.com>
2024-05-16 11:21:45 +0530

    Update App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,57 +1,65 @@
 import './App.css';
-import {useState} from "react";
+import { useState } from "react";
 import Button from "./components/button";
 import Input from './components/input';
 import * as math from "mathjs";
 
 const App = () => {
-
-  const[text , setText] = useState("")
-  const[result , setResult] = useState("")
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
 
   const addToText = (val) => {
-    setText((text) => [...text,val + " "]);
+    if (val === "Clear") {
+      resetInput();
+    } else {
+      setText((prevText) => prevText + val);
+    }
   };
 
   const calculateResult = () => {
-    const input = text.join("");
-    setResult(math.evaluate(input));
+    try {
+      console.log("Input:", text);
+      setResult(math.evaluate(text));
+    } catch (error) {
+      setResult("Error");
+      console.error("Calculation error:", error);
+    }
   };
 
   const resetInput = () => {
     setText("");
     setResult("");
-  }
+  };
 
   return (
     <div className="App">
       <div className="calc-wrapper">
-        <Input text = {text} result={result}/>
-        <div className = "rows ">
-          <Button symbol="7" handleClick={addToText}/>
-          <Button symbol="8" handleClick={addToText}/>
-          <Button symbol="9" handleClick={addToText}/>
-          <Button symbol="/" color = "#bdc0c7" handleClick={addToText} fontColor= "black"/>
+        <Input text={text} result={result} />
+        <div className="rows">
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color="#bdc0c7" handleClick={addToText} fontColor="black" />
         </div>
-        <div className = "rows ">
-          <Button symbol="4" handleClick={addToText}/>
-          <Button symbol="5" handleClick={addToText}/>
-          <Button symbol="6" handleClick={addToText}/>
-          <Button symbol="*" color = "#bdc0c7" handleClick={addToText} fontColor= "black"/>
+        <div className="rows">
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color="#bdc0c7" handleClick={addToText} fontColor="black" />
         </div>
-        <div className = "rows ">
-          <Button symbol="1" handleClick={addToText}/>
-          <Button symbol="2" handleClick={addToText}/>
-          <Button symbol="3" handleClick={addToText}/>
-          <Button symbol="-" color = "#bdc0c7" handleClick={addToText} fontColor= "black"/>
+        <div className="rows">
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="-" color="#bdc0c7" handleClick={addToText} fontColor="black" />
         </div>
-        <div className = "rows ">
-          <Button symbol="0" handleClick={addToText}/>
-          <Button symbol="." handleClick={addToText}/>
-          <Button symbol="=" handleClick={calculateResult}/>
-          <Button symbol="+" color = "#bdc0c7" handleClick={addToText} fontColor= "black"/>
+        <div className="rows">
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
+          <Button symbol="=" handleClick={calculateResult} />
+          <Button symbol="+" color="#bdc0c7" handleClick={addToText} fontColor="black" />
         </div>
-        <Button symbol="Clear" color = "#bdc0c7" handleClick={resetInput} fontColor= "black"/>
+        <Button symbol="Clear" color="#bdc0c7" handleClick={addToText} fontColor="black" />
       </div>
     </div>
   );
